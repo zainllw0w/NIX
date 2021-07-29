1b75215e804b2450de3e6e64744691acf3d12133
zainllw0w <77465388+zainllw0w@users.noreply.github.com>
2021-07-29 16:36:56 +0300

    Сделано задание 4

---
+++ 4.py
@@ -0,0 +1,12 @@
+def reg(myList):
+    return [x.upper() if x.find('price') >= 0 else x for x in myList ]
+
+
+myList = ['asdasd', 'asdpriceasd', 'price', 'asd price asda']
+
+
+print(reg(myList))
+
+
+
+
