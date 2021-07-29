16db8abb94e55cda6e6b560cedcfb8ce31a8209a
zainllw0w <77465388+zainllw0w@users.noreply.github.com>
2021-07-29 20:34:41 +0300

    сделано задание 5

--- 5.py
+++ 5.py
@@ -1,15 +1,25 @@
 def func(num, myList):
     newList = list()
-    if len(myList) < num:
-        print('Не может быть разбита больше чем есть в списке!')
+    y = 0
+
+    if len(myList) == 0:
+        print('Нет элементов')
+    elif len(myList) < num:
+        print('Количество элементов в списке меньше указанного')
+
     else:
         for i in range(num):
-            newList.append([myList[i]])
+           newList.append([])
+        for i in myList:
+            if y == num:
+                y = 0
+            newList[y].append(i)
+            y += 1
     return newList
 
 
 number = int(input('Введите число: '))
-myList = [123, 415, 34534]
+myList = [1, 2, 3]
 
 print(func(number, myList))
 
